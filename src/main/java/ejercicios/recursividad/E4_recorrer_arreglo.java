d566283ee39c2bc9e7305713bd7fcff4b317b50f
D'Angello <dangelloa.garcian@uqvirtual.edu.co>
2023-03-01 00:20:13 -0500

    E4 recorrer arreglo

--- src/main/java/ejercicios/recursividad/E4_recorrer_arreglo.java
+++ src/main/java/ejercicios/recursividad/E4_recorrer_arreglo.java
@@ -3,31 +3,34 @@ package ejercicios.recursividad;
 import javax.swing.*;
 
 public class E4_recorrer_arreglo {
+    /*
+    Recorra el arreglo desde la primera posición hasta la última posición y retorne el número de veces que se repite el elemento. Parámetros: vector[] (arreglo en el cual se encuentran los elementos), inicio (es el inicio del vector), fin (es el fin del vector), elem (es el número a buscar dentro del arreglo).
+     */
     public static void main(String[] args) {
-        int n = 2;
-        int suma = 0;
-        int[][] matriz = llenarMatriz(n);
-        suma = sumarDiagonalMatriz(matriz, n-1, n-1);
-        System.out.println("Suma: "+suma);
+        int n = 5;
+        int contador = 0;
+        int inicio = 0;
+        int fin = n-1;
+        int elem = 3;
+        int[] vector = llenarArreglo(n);
+        contador = recorrerArreglo(vector, inicio, fin, elem);
+        System.out.println("El número de veces que está "+elem+" es: "+contador);
     }
 
-    private static int sumarDiagonalMatriz(int[][] matriz, int i, int j) {
-        if(i >= 0) {
-            if(j >= 0) {
-                return sumarDiagonalMatriz(matriz, i, j-1) + matriz[i][j];
-            }
-            return sumarDiagonalMatriz(matriz, i-1, matriz.length-1);
+    private static int recorrerArreglo(int[] vector, int inicio, int fin, int elem) {
+        if(inicio <= fin) {
+            if(vector[inicio] == elem)
+                return recorrerArreglo(vector, inicio+1, fin, elem) +1;
+            return recorrerArreglo(vector, inicio+1, fin, elem);
         }
         return 0;
     }
 
-    private static int[][] llenarMatriz(int n) {
-        int[][] matriz = new int[n][n];
-        for(int i = 0; i<n; i++) {
-            for(int j = 0; j < n; j++) {
-                matriz[i][j] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número en la fila "+i+" y la columna "+j));
-            }
+    private static int[] llenarArreglo(int n) {
+        int [] vector = new int[n];
+        for(int i = 0; i < n; i++) {
+            vector[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número en la posición "+i));
         }
-        return matriz;
+        return vector;
     }
 }
