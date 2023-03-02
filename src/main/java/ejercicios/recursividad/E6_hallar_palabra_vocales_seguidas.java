2294ab501318a1d47355dcd1662687f504680945
D'Angello <dangelloa.garcian@uqvirtual.edu.co>
2023-03-01 19:19:12 -0500

    E5 y E6

--- src/main/java/ejercicios/recursividad/E6_hallar_palabra_vocales_seguidas.java
+++ src/main/java/ejercicios/recursividad/E6_hallar_palabra_vocales_seguidas.java
@@ -3,11 +3,12 @@ package ejercicios.recursividad;
 import java.util.ArrayList;
 
 public class E6_hallar_palabra_vocales_seguidas {
-
-
+    /*
+    Dada una matriz de String de 4 x 4 conformada por palabras, hallar las palabras que tienen dos vocales seguidas de forma recursiva y agregarlas a un ArrayList.
+     */
         public static void main(String[] args) {
             String[][] matriz = { { "casa", "perro", "auto", "mesa" }, { "letra", "libro", "avion", "flor" },
-                    { "taza", "agua", "computadoraaa", "papel" }, { "árbol", "oso", "gato", "sol" } };
+                    { "taza", "agua", "computadoraaa", "papel" }, { "árbol", "ooso", "gato", "sol" } };
 
             ArrayList<String> palabras = new ArrayList<>();
             recorrerMatriz(matriz, 0, 0, palabras);
@@ -16,35 +17,29 @@ public class E6_hallar_palabra_vocales_seguidas {
         }
 
         public static void recorrerMatriz(String[][] matriz, int i, int j, ArrayList<String> listaPalabras) {
-            if (i >= matriz.length) {
-                return;
+            if(i < matriz.length) {
+                if(j < matriz[i].length) {
+                    if (verificarVocalesSeguidas(matriz[i][j], 0))
+                        listaPalabras.add(matriz[i][j]);
+                    if(j == matriz[i].length - 1) {
+                        recorrerMatriz(matriz, i + 1, 0, listaPalabras);
+                    }
+                    recorrerMatriz(matriz, i, j + 1, listaPalabras);
+                }
             }
-
-            if (j >= matriz[i].length) {
-                recorrerMatriz(matriz, i + 1, 0, listaPalabras);
-                return;
-            }
-
-            if (verificarVocalesSeguidas(matriz[i][j], 0)) {
-                listaPalabras.add(matriz[i][j]);
-            }
-
-            recorrerMatriz(matriz, i, j + 1, listaPalabras);
         }
 
         public static boolean verificarVocalesSeguidas(String palabra, int index) {
-            if (index >= palabra.length() - 1) {
-                return false;
+            if(index < palabra.length()) {
+                if(index == palabra.length()-1)
+                    return false;
+                char letra1 = palabra.charAt(index);
+                char letra2 = palabra.charAt(index + 1);
+                if(esVocal(letra1) && esVocal(letra2))
+                    return true;
+                return verificarVocalesSeguidas(palabra, index + 1);
             }
-
-            char letra1 = palabra.charAt(index);
-            char letra2 = palabra.charAt(index + 1);
-
-            if (esVocal(letra1) && esVocal(letra2)) {
-                return true;
-            }
-
-            return verificarVocalesSeguidas(palabra, index + 1);
+            return false;
         }
 
         public static boolean esVocal(char letra) {
